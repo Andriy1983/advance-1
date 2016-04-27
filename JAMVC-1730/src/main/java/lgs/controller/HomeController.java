c56888469742670533cdfccdbdc5f7910a8640ec
Sergey Zhuravlyov <sergey.logos@gmail.com>
2016-04-27 19:03:58 +0300

    синг автор

--- JAMVC-1730/src/main/java/lgs/controller/HomeController.java
+++ JAMVC-1730/src/main/java/lgs/controller/HomeController.java
@@ -3,8 +3,10 @@ package lgs.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lgs.entity.Author;
 import lgs.service.AuthorService;
 @Controller
 public class HomeController { 
@@ -16,6 +18,16 @@ public class HomeController {
 	public String welcome(Model model){
 		model.addAttribute("key", authorService.getAllAuthors());
 		return "home";
+	}
+	
+	
+	@RequestMapping("/author-{id}")
+	public String singleAuthor(@PathVariable("id")int id,Model model){
+		Author author = authorService.getAuthorByID(id);
+		model.addAttribute("author", author);
+		
+		
+		return "singleAuthor";
 	}
 
 }
--- JAMVC-1730/src/main/webapp/view/singleAuthor.jsp
+++ JAMVC-1730/src/main/webapp/view/singleAuthor.jsp
@@ -1,28 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-    
-<%@taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<link rel="stylesheet" href='<c:url value=""></c:url>'>
 <title>Insert title here</title>
 </head>
 <body>
-HOME
 
-<table>
-<c:forEach items="${key}" var="author" >
-	<tr>
-		<td>${author.id}  aa</td>
-		<td><a href="author-${author.id}" style="color: red">${author.name}</a></td>
-	</tr>
-	
-</c:forEach>
-</table>
-
-<br>
+${author}
 
 </body>
 </html>
